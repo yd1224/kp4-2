8c88885e4a456ad78ed171fa91649b965baa316c
Yelyzaveta Demchenko <138761874+yd1224@users.noreply.github.com>
2023-10-18 23:11:50 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdbool.h>
+double getInput(const char *prompt);
+double getArr(void);
+bool isScientificNotation(const char *input);
 int main(void) {
   int size;
-    printf("Type the number of elements: ");
-    scanf("%d", &size);
-//    int size = 5;
-//    int arr[] = {2, 6, 7, 1, 4};
+    size = getInput("Type the number of elements: ");
     float arr[size];
 
         printf("Type %d elements:\n", size);
         for (int i = 0; i < size; i++) {
-            scanf("%f", &arr[i]);
+            arr[i] = getArr();
         }
 
 
@@ -31,4 +34,74 @@ int main(void) {
     printf("\n");
 
     return 0;
+}
+double getInput(const char *prompt) {
+    char input[15];
+    double number;
+
+    do {
+        printf("%s", prompt);
+        scanf("%s", input);
+
+        int invalidInput = 0;
+        for (int i = 0; input[i] != '\0'; i++) {
+            if (isdigit(input[i]) == 0 && input[i] != '.' && input[i] != '-' && !isScientificNotation(input)) {
+                printf("\nYour input is invalid\n");
+                invalidInput = 1;
+                break;
+            }
+        }
+
+        if (!invalidInput) {
+            number = atof(input);
+            break;
+        }
+    } while (1);
+
+    return number;
+}
+bool isScientificNotation(const char *input) {
+    int len = strlen(input);
+    int eCount = 0;
+    int digitsBeforeE = 0;
+    int digitsAfterE = 0;
+
+    for (int i = 0; i < len; i++) {
+        char c = input[i];
+        if (c == 'e' || c == 'E') {
+            eCount++;
+        } else if (isdigit(c) && eCount == 0) {
+            digitsBeforeE++;
+        } else if (isdigit(c) && eCount == 1) {
+            digitsAfterE++;
+        }
+    }
+
+
+    return (eCount == 1) && (digitsBeforeE > 0) && (digitsAfterE > 0);
+}
+double getArr(void){
+    char input[15];
+    double number;
+
+    do {
+
+        scanf("%s", input);
+
+        int invalidInput = 0;
+        for (int i = 0; input[i] != '\0'; i++) {
+            if (isdigit(input[i]) == 0 && input[i] != '.' && input[i] != '-' && !isScientificNotation(input)) {
+                printf("\nYour input is invalid, try again:\n");
+                invalidInput = 1;
+                break;
+            }
+        }
+
+        if (!invalidInput) {
+            number = atof(input);
+            break;
+        }
+    } while (1);
+
+    return number;
 }
